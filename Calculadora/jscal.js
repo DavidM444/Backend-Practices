062e86c8eb6069bd8b347933b0263de5c93f1792
DavidM444 <david541gamer@gmail.com>
2022-12-17 17:46:31 -0500

    Finish calculadora

--- Calculadora/jscal.js
+++ Calculadora/jscal.js
@@ -1,94 +1,44 @@
-
-// Variable que almacena el valor actual del display de la calculadora
 let currentValue = "0";
 
-// Variable que almacena el operador seleccionado
 let operator = null;
 
-// Variable que almacena el primer operando
 let operand1 = null;
 
-// Variable que almacena el segundo operando
 let operand2 = null;
 
-// Función que añade un dígito al valor actual del display
 function addDigit(digit) {
-    console.log(currentValue,operand1)
     if (currentValue === "0" && operand1 === null) {
         currentValue = String(digit);
-        console.log("if 1",currentValue)
-        
-    } else {
+    }else {
         currentValue += String(digit);
-        console.log(currentValue)
-
-        if (operand1 === null) {
-            operand1 = parseInt(currentValue)
-            
-    
-        }else{
-            operand2 = parseInt(currentValue)
-        } 
-        console.log("o1",operand1, "o2",operand2,"cy",currentValue, "oper",operator )
     }
-    
-    console.log(operand1,operand2,currentValue)
-
-    
+    if(operator !== null){
+        operand2 = parseInt(currentValue)
+    }else{
+    operand1 = parseInt(currentValue)
+    }
     updateDisplay();
-    
-
-  
 };
-
-
-
-
-
-
-
 
 function updateDisplay(){
     let displayElement = document.getElementById("display")
     displayElement.value = currentValue
 };
 
-
-
-// Función que añade un operador
 function addOperator(newOperator) {
-    console.log(currentValue,newOperator)
-    
     currentValue += newOperator;
     operator = newOperator;
-    
-    // Si no se ha seleccionado un operador aún, establecer el operador seleccionado
-    if (operator !== null && operand1 !== null && operand2 === null) {
-        console.log(operand1)
-        console.log(currentValue)
-        
-        currentValue = ""
-        //updateDisplay()
-     
-        console.log(operator,newOperator,operand1)
-        console.log(currentValue)
-        }
-        else {
-            compute()
-   
-        }
     updateDisplay()
-        
-    }
+    if (operator !== null && operand1 !== null) {
+        currentValue = ""
+        }else {
+            compute()
+        }
+};
     
-    
-
-// Función que realiza el cálculo
 function compute() {
-    
-    if (operand1 !== null && operand2 !== null && operator !== null) {
+    if (operand2 !== null && operator !== null) {
         let result = 0;
-        console.log(currentValue, operand1,operand2,operator) 
         switch (operator) {  
             case "+":
                 result = operand1 + operand2;
@@ -96,7 +46,7 @@ function compute() {
             case "-":
                 result = operand1 - operand2;
                 break;
-            case "x":
+            case "*":
                 result = operand1 * operand2;
                 break;
             case "/":
@@ -109,37 +59,26 @@ function compute() {
                 result = Math.pow(operand1,operand2)
                 break;
             case "√":
-                result = Math.sqrt(operand1);
+                result = Math.sqrt(operand2);
                 break;
-            
             }
-
-
-             // Actualizar el valor del display con el resultado del cálculo
-            console.log(result)
             currentValue = String(result);
-            console.log(currentValue)
-            
+            currentValue = operand1 +  operator + operand2 +"="+result
+            if(operator === "√"){
+                currentValue = operator + operand2 +"="+result
+            }
             updateDisplay();
-    
-      // Limpiar el estado de la calculadora
-     
     }else {
         currentValue = "0"
-        console.log("manda el iguyal ", currentValue)
     }  
 }
-
-// Función que limpia el estado de la calculadora
-
-function clear() {
-    currentValue = ""
-    updateDisplay();
-}
-      // Limpiar los valores de las variables
+function clean(){
+    currentValue = "0"
+    operator = null
+    operand1 = null
+    operand2 = null  
+    updateDisplay()
+      
     
-  
-    // Actualizar el display para reflejar el valor inicial de la calculadora
-    
+};
 
-  
