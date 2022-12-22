10847370c86c4d561ab53c18e69415f465d4ebca
DavidM444 <david541gamer@gmail.com>
2022-12-22 11:17:51 -0500

    position boxes

---
+++ React/appreact/src/components/Container.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Container = () => {
+    return(
+        <html>
+             <body>
+                <header>
+                <div>
+                    <h2>Portafolio de David</h2>
+                </div>
+                </header>
+                <section id="first">
+                    <div className="box"><div className="subbox">
+                        <div className="capa"/>
+                        </div>
+                    </div>
+
+                    <div className="box">
+                        <div className="subbox">
+                            <div className="capa"/>
+                        </div>
+                    </div>
+                    <div className="box"><div className="subbox">
+                    
+                        <div className="capa"/>
+                        </div>
+                    </div>
+
+                            
+                    <div className="box"><div className="subbox">
+                        <div className="capa"/>
+                        </div>
+                    </div>
+
+                </section>
+        
+            </body>
+        </html>
+       
+        
+    )
+}
+export default Container
